fa20396600f1810c909691f2e5e8f617e9e5252a
Jo-kyeong-ho <44523148+Jo-kyeong-ho@users.noreply.github.com>
2019-05-09 15:50:01 +0900

    Update dbtest.java

--- java/dbtest.java
+++ java/dbtest.java
@@ -18,7 +18,7 @@ public class dbtest extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dbtest);
         add=findViewById(R.id.add);
         confirm=findViewById(R.id.confirm);
         db=openOrCreateDatabase("testdb", Activity.MODE_PRIVATE,null);
