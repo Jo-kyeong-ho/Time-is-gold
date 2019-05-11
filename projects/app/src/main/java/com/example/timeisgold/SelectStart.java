b1f8cd2f6478b72c60d991f1e585e441de3b99bb
seokhyeonSong <44026660+seokhyeonSong@users.noreply.github.com>
2019-05-11 14:36:00 +0900

    Add files via upload

---
+++ projects/app/src/main/java/com/example/timeisgold/SelectStart.java
@@ -0,0 +1,34 @@
+package com.example.timeisgold;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_start);
+
+        Button value = findViewById(R.id.valueBtn);
+        Button waste = findViewById(R.id.wasteBtn);
+
+        value.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent valueInt = new Intent(getApplicationContext(), StartValue.class);
+                startActivity(valueInt);
+            }
+        });
+        waste.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent wasteInt = new Intent(getApplicationContext(), StartWaste.class);
+                startActivity(wasteInt);
+            }
+        });
+    }
+}
