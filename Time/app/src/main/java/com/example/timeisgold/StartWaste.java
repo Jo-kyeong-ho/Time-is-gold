b99a0f89928345d15615b929752595a754c2a079
seokhyeonSong <44026660+seokhyeonSong@users.noreply.github.com>
2019-05-11 16:44:47 +0900

    Add files via upload

---
+++ Time/app/src/main/java/com/example/timeisgold/StartWaste.java
@@ -0,0 +1,13 @@
+package com.example.timeisgold;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartWaste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_waste);
+    }
+}
