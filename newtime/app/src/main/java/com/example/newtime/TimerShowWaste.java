993a27d122e78c0bb03668861775c24e853b3f9c
Jo-kyeong-ho <44523148+Jo-kyeong-ho@users.noreply.github.com>
2019-05-11 14:32:51 +0900

    Add files via upload

---
+++ newtime/app/src/main/java/com/example/newtime/TimerShowWaste.java
@@ -0,0 +1,26 @@
+package com.example.newtime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class TimerShowWaste extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_wastetimer);
+
+        Button decResult = findViewById(R.id.decBtn);
+
+        decResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent decInt = new Intent(getApplicationContext(), WasteResult.class);
+                startActivity(decInt);
+            }
+        });
+    }
+}
